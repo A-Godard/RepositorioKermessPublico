042b3cbdbc445bf61ea178bfe7f8f3dd8bd44f20
JABV97 <jorgebv_97@hotmail.com>
2021-10-28 00:06:11 -0600

    creacion tabla y reporte para comunidad

---
+++ SolucionKermesseGrupo2/Controllers/ComunidadesController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.Reporting.WebForms;
+using SolucionKermesseGrupo2.Models;
+
+namespace SolucionKermesseGrupo2.Controllers
+{
+    public class ComunidadesController : Controller
+    {
+        private BDKermesseEntities db = new BDKermesseEntities();
+
+        public ActionResult verReporte(string tipo)
+        {
+            LocalReport rpt = new LocalReport();
+            string mt, enc, f;
+            string[] s;
+            Warning[] w;
+
+
+            string ruta = Path.Combine(Server.MapPath("~/Reportes"), "RptComunidad.rdlc");
+            rpt.ReportPath = ruta;
+
+            BDKermesseEntities modelo = new BDKermesseEntities();
+            List<Comunidad> ls = new List<Comunidad>();
+            ls = modelo.Comunidad.ToList();
+
+
+            ReportDataSource rds = new ReportDataSource("DsComunidad", ls);
+            rpt.DataSources.Add(rds);
+
+            byte[] b = rpt.Render(tipo, null, out mt, out enc, out f, out s, out w);
+
+            return File(b, mt);
+        }
+
+        // GET: Comunidades
+        public ActionResult Index()
+        {
+            return View(db.Comunidad.ToList());
+        }
+
+        // GET: Comunidades/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Comunidad comunidad = db.Comunidad.Find(id);
+            if (comunidad == null)
+            {
+                return HttpNotFound();
+            }
+            return View(comunidad);
+        }
+
+        // GET: Comunidades/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Comunidades/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "idComunidad,nombre,responsble,descContribucion,estado")] Comunidad comunidad)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Comunidad.Add(comunidad);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(comunidad);
+        }
+
+        // GET: Comunidades/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Comunidad comunidad = db.Comunidad.Find(id);
+            if (comunidad == null)
+            {
+                return HttpNotFound();
+            }
+            return View(comunidad);
+        }
+
+        // POST: Comunidades/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "idComunidad,nombre,responsble,descContribucion,estado")] Comunidad comunidad)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(comunidad).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(comunidad);
+        }
+
+        // GET: Comunidades/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Comunidad comunidad = db.Comunidad.Find(id);
+            if (comunidad == null)
+            {
+                return HttpNotFound();
+            }
+            return View(comunidad);
+        }
+
+        // POST: Comunidades/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Comunidad comunidad = db.Comunidad.Find(id);
+            db.Comunidad.Remove(comunidad);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
