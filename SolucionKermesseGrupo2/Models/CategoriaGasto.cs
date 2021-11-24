3ca461255b8e1c6897f1c380961311723f4afbfe
A-Godard <saballosandres64@gmail.com>
2021-11-24 04:23:32 -0600

    Carpinteria y edicion en todo el CRUD de cat gasto

--- SolucionKermesseGrupo2/Models/CategoriaGasto.cs
+++ SolucionKermesseGrupo2/Models/CategoriaGasto.cs
@@ -11,7 +11,8 @@ namespace SolucionKermesseGrupo2.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+
     public partial class CategoriaGasto
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -21,7 +22,11 @@ namespace SolucionKermesseGrupo2.Models
         }
     
         public int idCatGasto { get; set; }
+       [Display (Name="Nombre de Categoria")]
+       [Required]
         public string nombreCategoria { get; set; }
+        [Display(Name = "Descripción")]
+        [Required]
         public string descripcion { get; set; }
         public int estado { get; set; }
     
